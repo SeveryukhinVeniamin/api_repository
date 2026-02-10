a1943105779e3709b852573ca041c6c1febd313e
SeveryukhinVeniamin <severyukhinvd@yandex.ru>
2026-02-10 15:53:56 +0300

    With drawing_map

---
+++ drawing_map.py
@@ -0,0 +1,24 @@
+import requests
+import sys
+
+
+def getImage(ll, z, theme='light'):
+    server_address = 'https://static-maps.yandex.ru/v1?'
+    api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+    params = f'll={ll[0]},{ll[1]}&z={z[0]}&theme={theme}'
+
+    map_request = f"{server_address}{params}&apikey={api_key}"
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+    return map_file
